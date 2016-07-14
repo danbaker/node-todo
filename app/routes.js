2be78e6091b881e0f7f50d3377234d2c5081f6c8
Dan Baker <dbmail@bakercrew.com>
2016-07-14 14:02:08 -0600

    allow for repeating an request -- added an route that shows the current aggregated results

--- app/routes.js
+++ app/routes.js
@@ -9,25 +9,35 @@ module.exports = function (app) {
   });
 
   app.get('/runrequest', function (req, res) {
+    var waitseconds = req.query.waitseconds || 60;  // repeat every 60 seconds
+    var repeattimes = req.query.repeattimes || 0;   // default to NOT repeat
     var options = {
       host: req.query.host || 'www.google.com',
       port: req.query.port || 80,
       path: req.query.path || '/index.html'
     };
-    var start = process.hrtime();
-    httpRequest(options, function(body) {
-      var timeinfo = process.hrtime(start); // [0] = seconds [1] = nanoseconds
-      var elapsedms = timeinfo[0] * 1000 + process.hrtime(start)[1] / 1000000; // ms
-      requestsms.push(elapsedms);
-      res.send("http://" + options.host + ":" + options.port + "/" + options.path + " = OK -- in " + elapsedms + "ms");
-    }, function(errMessage) {
-      res.send("ERROR - " + errMessage);
-    })
+    runRepeatedRequests(options, res, waitseconds, repeattimes);
   });
 
   app.get('/showrequests', function (req, res) {
     var msg = "The last many requests: ";
-    msg += requestsms.length;
+    var shortest = requestsms[0];
+    var longest = requestsms[0];
+    var totalms = 0;
+    var totaln = requestsms.length;
+    for(var i=0; i<totaln; i++) {
+      var ms = requestsms[i];
+      totalms += ms;
+      if (ms < shortest) shortest = ms;
+      if (ms > longest) longest = ms;
+    }
+    msg += "</br><table>";
+    msg += "<tr><td>Total Requests</td><td style='text-align:right'>" + parseInt(totaln) + "</td></tr>";
+    msg += "<tr><td>Total Time</td><td style='text-align:right'>" + parseInt(totalms) + "ms</td></tr>";
+    msg += "<tr><td>Quickest</td><td style='text-align:right'>" + parseInt(shortest) + "ms</td></tr>";
+    msg += "<tr><td>Slowest</td><td style='text-align:right'>" + parseInt(longest) + "ms</td></tr>";
+    msg += "<tr><td>Average</td><td style='text-align:right'>" + parseInt(totalms/totaln) + "ms</td></tr>";
+    msg += "</table>";
     res.send(msg);
   });
 
@@ -52,4 +62,23 @@ function httpRequest(options, fncOK, fncERROR)
     console.log("Got error: " + e.message);
     fncERROR(e.message);
   });
+}
+
+function runRepeatedRequests(options, res, waitseconds, repeattimes)
+{
+  var start = process.hrtime();
+  httpRequest(options, function(body) {
+    var timeinfo = process.hrtime(start); // [0] = seconds [1] = nanoseconds
+    var elapsedms = timeinfo[0] * 1000 + process.hrtime(start)[1] / 1000000; // ms
+    requestsms.push(elapsedms);
+    if (res) res.send("http://" + options.host + ":" + options.port + "/" + options.path + " = OK -- in " + elapsedms + "ms");
+    if (--repeattimes > 0) {
+      setTimeout(function() {
+        runRepeatedRequests(options, undefined, waitseconds*1000, repeattimes);
+      }, waitseconds);
+    }
+  }, function(errMessage) {
+    if (res) res.send("ERROR - " + errMessage);
+  });
+
 }
